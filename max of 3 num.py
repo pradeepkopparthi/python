69c881fb96e58e57bbb8044a55f8547cb0697d0a
pradeepkopparthi <126454952+pradeepkopparthi@users.noreply.github.com>
2023-02-27 14:14:16 +0530

    Add files via upload

---
+++ max of 3 num.py
@@ -0,0 +1,9 @@
+a=int(input('enter value of a='))
+b=int(input('enter value of b='))
+c=int(input('enter value of c='))
+if a>b and a>c:
+    print("a is max")
+elif b>c:
+    print('b is max')
+else:
+    print("c is max")
