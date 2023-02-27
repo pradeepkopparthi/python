76c4b1f62324c9db95f857a141632a590a359c2f
pradeepkopparthi <126454952+pradeepkopparthi@users.noreply.github.com>
2023-02-27 14:32:41 +0530

    Add files via upload

---
+++ to print n natural numbers.py
@@ -0,0 +1,8 @@
+#to print first n natural numbers using while loop
+n=int(input('enter the value of n:'))
+i=1
+while i<=n:
+    print(i)
+    i+=1
+else:
+    print('n natural numbers are printed')
