d09830a3b2be89e591fcd3c6082ebdc543d2a3ab
pradeepkopparthi <126454952+pradeepkopparthi@users.noreply.github.com>
2023-02-27 14:40:18 +0530

    Add files via upload

---
+++ sum of n natural numbers.py
@@ -0,0 +1,11 @@
+#to find sum of  first n natural numbers using while loop
+n=int(input('enter the value of n:'))
+i=1
+sum=0
+while i<=n:
+    sum=sum+i
+    i+=1
+print("sum of n natural numbers=")
+print(sum)
+    
+    
